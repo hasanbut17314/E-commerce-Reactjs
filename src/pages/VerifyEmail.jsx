0b79aef589e12ca0ccb07fb0370da5db09d321fe
Hassan Ali <139656401+hasanbut17314@users.noreply.github.com>
2024-09-14 16:49:59 +0500

    fixed verify email function

--- src/pages/VerifyEmail.jsx
+++ src/pages/VerifyEmail.jsx
@@ -6,36 +6,31 @@ import notify from '../utils/notify';
 const VerifyEmail = () => {
 
     const navigate = useNavigate();
-    const {token} = useParams();
-    const [verifyEmail, {isLoading}] = useVerifyEmailMutation();
+    const { token } = useParams();
+    const [verifyEmail, { isLoading }] = useVerifyEmailMutation();
 
     const verify = async () => {
         try {
-            response = await verifyEmail(token).unwrap();
-            if(response.status === 200) {
-                notify.success(response.message);
-                navigate('/login');
-            } else if(response.status === 404) {
-                notify.error('Unable to verify email. Your Token may be expired. Please try again.');
-            }
+            await verifyEmail(token).unwrap();
+            notify.success('Email verified successfully');
+            navigate('/login');
         } catch (error) {
-            notify.error(error.data.message);
-            console.log(error);
+            notify.error('Unable to verify email. Your Token may be expired. Please try again');
         }
     }
 
-  return (
-    <div className='text-center mt-5'>
-        <h1 className='text-xl'>Click on Verify button to verify your email</h1>
-        <button 
-        className='btn bg-green-400 px-5 py-2 rounded-md text-white font-semibold mt-3'
-        onClick={verify}
-        disabled={isLoading}
-        >
-        Verify
-        </button>
-    </div>
-  )
+    return (
+        <div className='text-center mt-5'>
+            <h1 className='text-xl'>Click on Verify button to verify your email</h1>
+            <button
+                className='btn bg-green-400 px-5 py-2 rounded-md text-white font-semibold mt-3'
+                onClick={verify}
+                disabled={isLoading}
+            >
+                Verify
+            </button>
+        </div>
+    )
 }
 
 export default VerifyEmail
