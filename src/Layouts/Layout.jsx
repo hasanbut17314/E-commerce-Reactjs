38e419248a35f30fce89d011e611548582dbe796
hasanbut17314 <hasanbut17314@gmail.com>
2025-09-16 11:26:06 +0500

    added log API

--- src/Layouts/Layout.jsx
+++ src/Layouts/Layout.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { CssBaseline } from '@mui/material';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
@@ -6,6 +6,19 @@ import { Outlet } from 'react-router-dom';
 import ScrollTop from '../components/ScrollTop';
 
 function Layout() {
+
+  useEffect(() => {
+    const insertLog = async () => {
+      try {
+        const response = await fetch('https://martyz-backend.vercel.app/track')
+        console.log('Log inserted:', response.status);
+      } catch (error) {
+        console.error('Error inserting log:', error);
+      }
+    }
+
+    insertLog();
+  }, []);
   return (
     <>
       <CssBaseline />
